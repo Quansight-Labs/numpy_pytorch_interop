c0fade37493738b8406c5482a5c5346470fa5db3
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2023-01-28 19:43:27 +0300

    MAINT: run lint / pre-commit

--- torch_np/testing/__init__.py
+++ torch_np/testing/__init__.py
@@ -3,12 +3,12 @@ from .utils import (
     assert_,
     assert_allclose,
     assert_almost_equal,
-    assert_array_equal,
     assert_array_almost_equal,
-    assert_raises_regex,
-    suppress_warnings,
+    assert_array_equal,
     assert_equal,
+    assert_raises_regex,
     assert_warns,
+    suppress_warnings,
 )
 
 # from .testing import assert_allclose    # FIXME
