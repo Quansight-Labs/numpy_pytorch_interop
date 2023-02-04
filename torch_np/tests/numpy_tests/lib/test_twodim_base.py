c0ae10009c8bdc4e2316455d2f8cb139577db654
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2023-02-04 21:50:38 +0300

    WIP: make test_twodim_base importable

--- torch_np/tests/numpy_tests/lib/test_twodim_base.py
+++ torch_np/tests/numpy_tests/lib/test_twodim_base.py
@@ -1,22 +1,19 @@
 """Test functions for matrix module
 
 """
-from numpy.testing import (
-    assert_equal, assert_array_equal, assert_array_max_ulp,
-    assert_array_almost_equal, assert_raises, assert_
+from torch_np.testing import (
+    assert_equal, assert_array_equal, # assert_array_max_ulp,
+    assert_array_almost_equal, assert_
     )
+from pytest import raises as assert_raises
 
-from numpy import (
+from torch_np import (
     arange, add, fliplr, flipud, zeros, ones, eye, array, diag, histogram2d,
     tri, mask_indices, triu_indices, triu_indices_from, tril_indices,
     tril_indices_from, vander,
     )
 
-import numpy as np
-
-
-from numpy.core.tests.test_overrides import requires_array_function
-
+import torch_np as np
 
 import pytest
 
@@ -282,27 +279,6 @@ class TestHistogram2d:
              [0., 0., 0., 0., 0., 0., 0., 0., 0., 1.]])
         assert_array_equal(H, answer)
         assert_array_equal(xe, array([0., 0.25, 0.5, 0.75, 1]))
-
-    @requires_array_function
-    def test_dispatch(self):
-        class ShouldDispatch:
-            def __array_function__(self, function, types, args, kwargs):
-                return types, args, kwargs
-
-        xy = [1, 2]
-        s_d = ShouldDispatch()
-        r = histogram2d(s_d, xy)
-        # Cannot use assert_equal since that dispatches...
-        assert_(r == ((ShouldDispatch,), (s_d, xy), {}))
-        r = histogram2d(xy, s_d)
-        assert_(r == ((ShouldDispatch,), (xy, s_d), {}))
-        r = histogram2d(xy, xy, bins=s_d)
-        assert_(r, ((ShouldDispatch,), (xy, xy), dict(bins=s_d)))
-        r = histogram2d(xy, xy, bins=[s_d, 5])
-        assert_(r, ((ShouldDispatch,), (xy, xy), dict(bins=[s_d, 5])))
-        assert_raises(Exception, histogram2d, xy, xy, bins=[s_d])
-        r = histogram2d(xy, xy, weights=s_d)
-        assert_(r, ((ShouldDispatch,), (xy, xy), dict(weights=s_d)))
 
     @pytest.mark.parametrize(("x_len", "y_len"), [(10, 11), (20, 19)])
     def test_bad_length(self, x_len, y_len):
