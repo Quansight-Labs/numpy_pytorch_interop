85d1ba66fa5d874ff87d67559fe86693444b31a7
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2023-02-17 18:19:25 +0300

    TST: test isreal(real array)

--- torch_np/tests/numpy_tests/lib/test_type_check.py
+++ torch_np/tests/numpy_tests/lib/test_type_check.py
@@ -157,7 +157,6 @@ class TestImag:
         assert_(not isinstance(out, np.ndarray))
 
 
-@pytest.mark.xfail(reason="not implemented")
 class TestIscomplex:
 
     def test_fail(self):
@@ -171,7 +170,6 @@ class TestIscomplex:
         assert_array_equal(res, [1, 0, 0])
 
 
-@pytest.mark.xfail(reason="not implemented")
 class TestIsreal:
 
     def test_pass(self):
@@ -183,6 +181,11 @@ class TestIsreal:
         z = np.array([-1j, 1, 0])
         res = isreal(z)
         assert_array_equal(res, [0, 1, 1])
+
+    def test_isreal_real(self):
+        z = np.array([-1, 0, 1])
+        res = isreal(z)
+        assert res.all()
 
 
 @pytest.mark.xfail(reason="not implemented")
@@ -201,42 +204,6 @@ class TestIscomplexobj:
     def test_list(self):
         assert_(iscomplexobj([3, 1+0j, True]))
         assert_(not iscomplexobj([3, 1, True]))
-
-    def test_duck(self):
-        class DummyComplexArray:
-            @property
-            def dtype(self):
-                return np.dtype(complex)
-        dummy = DummyComplexArray()
-        assert_(iscomplexobj(dummy))
-
-    def test_pandas_duck(self):
-        # This tests a custom np.dtype duck-typed class, such as used by pandas
-        # (pandas.core.dtypes)
-        class PdComplex(np.complex128):
-            pass
-        class PdDtype:
-            name = 'category'
-            names = None
-            type = PdComplex
-            kind = 'c'
-            str = '<c16'
-            base = np.dtype('complex128')
-        class DummyPd:
-            @property
-            def dtype(self):
-                return PdDtype
-        dummy = DummyPd()
-        assert_(iscomplexobj(dummy))
-
-    def test_custom_dtype_duck(self):
-        class MyArray(list):
-            @property
-            def dtype(self):
-                return complex
-
-        a = MyArray([1+0j, 2+0j, 3+0j])
-        assert_(iscomplexobj(a))
 
 
 @pytest.mark.xfail(reason="not implemented")
