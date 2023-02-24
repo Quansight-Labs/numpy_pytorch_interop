c80f30ddacaa0ff1617c3dc49b194662677c9c08
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2023-02-24 21:12:59 +0300

    TST: un-xfail test_outer

--- torch_np/tests/numpy_tests/core/test_numeric.py
+++ torch_np/tests/numpy_tests/core/test_numeric.py
@@ -2820,13 +2820,12 @@ class TestCross:
         assert_equal(np.cross(u, v), -z)
 
 
-@pytest.mark.xfail(reason="TODO")
 def test_outer_out_param():
     arr1 = np.ones((5,))
     arr2 = np.ones((2,))
     arr3 = np.linspace(-2, 2, 5)
-    out1 = np.ndarray(shape=(5,5))
-    out2 = np.ndarray(shape=(2, 5))
+    out1 = np.empty(shape=(5,5))
+    out2 = np.empty(shape=(2, 5))
     res1 = np.outer(arr1, arr3, out1)
     assert_equal(res1, out1)
     assert_equal(np.outer(arr2, arr3, out2), out2)
