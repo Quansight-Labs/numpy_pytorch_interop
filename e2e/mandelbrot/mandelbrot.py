d727efff309c71e264072970c2b2ee16d6dd25c8
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2023-07-17 15:47:37 +0300

    e2e: measure speedup

--- e2e/mandelbrot/mandelbrot.py
+++ e2e/mandelbrot/mandelbrot.py
@@ -5,6 +5,9 @@
 # -----------------------------------------------------------------------------
 import math
 import numpy as np
+import time
+
+# need to import before torch
 from matplotlib import colors
 import matplotlib.pyplot as plt
 
@@ -13,6 +16,35 @@ torch.set_default_device("cpu")
 import torch._dynamo.config as cfg
 cfg.numpy_ndarray_as_tensor = True
 
+
+# ### Original NumPy version. ###
+
+def mandelbrot(xmin, xmax, ymin, ymax, xn, yn, maxiter, horizon=2.0):
+    # Adapted from https://www.ibm.com/developerworks/community/blogs/jfp/...
+    #              .../entry/How_To_Compute_Mandelbrodt_Set_Quickly?lang=en
+    X = np.linspace(xmin, xmax, xn, dtype=np.float32)
+    Y = np.linspace(ymin, ymax, yn, dtype=np.float32)
+    C = X + Y[:,None]*1j
+    N = np.zeros(C.shape, dtype=int)
+    Z = np.zeros(C.shape, np.complex64)
+    for n in range(maxiter):
+        I = np.less(abs(Z), horizon)
+        N[I] = n
+        Z[I] = Z[I]**2 + C[I]
+    N[N == maxiter-1] = 0
+    return Z, N
+
+
+
+# ### Compiled analog. ###
+
+# For torch.Dynamo, need to work around
+#    1. Complex numbers: add a trailing length-2 dimension for Re and Im parts.
+#    2. Avoid fancy indexing: use with np.where instead to avoid data dependency
+#
+# Also:
+#    1. Only compile the inner loop, to keep compile time and memory consumption
+#       under control (otherwise, can run into OOM while compiling)
 
 def abs2(a):
     r"""abs(a) replacement."""
@@ -50,20 +82,10 @@ def mandelbrot_c(xmin, xmax, ymin, ymax, xn, yn, horizon=2**10, maxiter=5):
     return Z, N
 
 
-if __name__ == '__main__':
-    # start up
-    xmax, xmin, xn = -2.25, 0.75, 3000 // 2
-    ymax, ymin, yn = -1.25, 1.25, 2500 // 2
 
-    maxiter = 200
-    horizon = 2**40
+# plot a nice figure
+def visualize(Z, N, horizon, xn, yn):
     log_horizon = math.log(horizon, 2)
-
-    # simulate
-    Z, N = mandelbrot_c(xmin, xmax, ymin, ymax, xn, yn, maxiter=maxiter)
-
-    # Visualization
-    Z = Z[..., 0] + 1j*Z[..., 1]
     M = np.nan_to_num(N + 1 - np.log(np.log(abs(Z)))/np.log(2) + log_horizon)
 
     dpi = 72
@@ -82,4 +104,38 @@ if __name__ == '__main__':
     ax.set_yticks([])
     plt.savefig("mandelbrot.png")
     plt.show()
+
+
+
+if __name__ == '__main__':
+    # start up
+    xmax, xmin, xn = -2.25, 0.75, 3000 // 2
+    ymax, ymin, yn = -1.25, 1.25, 2500 // 2
+
+    maxiter = 200
+    horizon = 2**10
+
+    # time numpy
+    start_time = time.time()
+    Z, N = mandelbrot(xmin, xmax, ymin, ymax, xn, yn, horizon=horizon, maxiter=maxiter)
+    end_time = time.time()
+    numpy_time = end_time - start_time
+    print("\n\nnumpy:    elapsed=", numpy_time)   
+
+
+    # compile, warm up, time
+    for _ in range(3):
+        mandelbrot_c(xmin, xmax, ymin, ymax, xn, yn, horizon=horizon, maxiter=maxiter)
+
+    # measure
+    start_time = time.time()
+    Z, N = mandelbrot_c(xmin, xmax, ymin, ymax, xn, yn, horizon=horizon, maxiter=maxiter)
+    end_time = time.time()
+    compiled_time = end_time - start_time
+    print("compiled: elapsed=", compiled_time, '  speedup = ', numpy_time / compiled_time)
+
+    # Visualization
+    Z = Z[..., 0] + 1j*Z[..., 1]
+    visualize(Z, N, horizon, xn, yn)
+
 
